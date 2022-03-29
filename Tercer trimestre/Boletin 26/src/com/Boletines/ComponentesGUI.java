9e729d9b7b751a971bcd0058a807ff4bb7c92315
Iker Rodriguez <irodrigueziglesias@danielcastelao.org>
2022-03-29 10:29:21 +0200

    Boletin 26 con botones funcionales

--- Tercer trimestre/Boletin 26/src/com/Boletines/ComponentesGUI.java
+++ Tercer trimestre/Boletin 26/src/com/Boletines/ComponentesGUI.java
@@ -1,8 +1,13 @@
 package com.Boletines;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class ComponentesGUI {
+public class ComponentesGUI extends MouseAdapter implements ActionListener {
     private JFrame marco;
     private JPanel panel;
     private JButton boton1,boton2;
@@ -15,16 +20,25 @@ public class ComponentesGUI {
         marco.setSize(600,500);
         panel = new JPanel();
         panel.setSize(600,500);
-        boton1 = new JButton("Aceptar");
+        boton1 = new JButton("Enviar");
+
+
         boton2 = new JButton("Limpiar");
+
+
         nombre = new JLabel("Nombre");
+        nombre.setFont(new Font("Roboto",Font.BOLD,15));
         password = new JLabel("Password");
-        jta = new JTextArea("Area de texto");
+        password.setFont(new Font("Roboto",Font.BOLD,15));
+        jta = new JTextArea("");
         jta.setColumns(40);
         jta.setRows(10);
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+        jta.setBorder(border);
         jtf1 = new JTextField();
+
         jtf1.setColumns(15);
-        jtf2 = new JTextField();
+        jtf2 = new JPasswordField();
         jtf2.setColumns(15);
     }
 
@@ -46,7 +60,23 @@ public class ComponentesGUI {
         boton2.setBounds(300,500,10,15);
         panel.add(boton1);
         panel.add(boton2);
+        boton1.addActionListener((ActionListener) this);
+        boton2.addActionListener(this);
         marco.add(panel);
+
         this.cerrarVentana();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object objeto = e.getSource();
+        if(objeto == boton1){
+            jta.setText("Hola " + jtf1.getText() + "!");
+
+        }
+        else if (objeto == boton2){
+            jtf1.setText("");
+            jtf2.setText("");
+        }
     }
 }
